6e9fd9d45c9a701b3633e619a1fb844e34cdd4e3
Grant K <grantkveton@msn.com>
2020-03-04 16:00:53 -0500

    lighttest Made

---
+++ lightTest.py
@@ -0,0 +1,20 @@
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1015(i2c)
+#single ended analog input mode
+chan1 = AnalogIn(ads, ADS.P0)
+
+
+def getLight():
+	lightVal = 0
+	for i in range(5):
+		lightVal += chan1.value
+	return lightVal/5
+
+while True:
+    print(getLight())
