e7bdfa7802faeba1aa5c3f4c16d95090a1690acc
Grant K <grantkveton@msn.com>
2020-03-04 17:22:25 -0500

    added GPIO lib

--- lightTest.py
+++ lightTest.py
@@ -3,6 +3,8 @@ import board
 import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+import sys, time
+import RPi.GPIO as GPIO
 
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1015(i2c)
